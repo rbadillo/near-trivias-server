a44f5376327a66b0269947fe8136943fe95e5186
Roberto Badillo <rbadillo@gatech.edu>
2018-07-20 23:22:47 -0400

    Adding Mysql

--- registerServer.js
+++ registerServer.js
@@ -4,6 +4,16 @@ var http = require('http').Server(app);
 var port = process.env.PORT || 12000;
 var bodyParser = require('body-parser')
 var sha256 = require('js-sha256');
+var mysql = require('mysql');
+
+// Mysql
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : '127.0.0.1',
+  user            : 'root',
+  password        : 'EstaTrivialDb!',
+  database        : 'trivias_near'
+});
 
 app.use(bodyParser.json());
 
@@ -16,11 +26,32 @@ app.get('/', function(req, res){
 app.post('/login', function(req, res){
   console.log(req.body)
 
+  var player_email = req.body.email 
+  var player_password = req.body.password
+
   var response = {
-    "msg":"success"
+    "msg":""
   }
 
-  res.status(400).json(response)
+  var user_query = "Select id from users where email = ? and password = ?"
+  var query_values = [player_email,player_password]
+
+  pool.query(user_query, query_values , function (error, results, fields) {
+    if (error)
+    {
+      response['msg']="Hubo un error en el servidor, Porfavor intenta de nuevo"
+      res.status(500).json(response)
+    }
+
+    console.log(results)
+    console.log(results.length)
+    console.log(fields)
+
+    response['msg']="Bienvenido a Trivias Near"
+    res.status(200).json(response)
+
+  });
+
 });
 
 // Player Register
